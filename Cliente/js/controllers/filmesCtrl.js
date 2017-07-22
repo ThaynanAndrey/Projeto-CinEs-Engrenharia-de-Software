d10f1e0e0b2494bd40254e4f5b99db6cf56f5abd
Giuseppe <giuseppe.mongiovi@ccc.ufcg.edu.br>
2017-07-22 18:21:32 -0300

    começo de implementação pra filtrar filmes

--- Cliente/js/controllers/filmesCtrl.js
+++ Cliente/js/controllers/filmesCtrl.js
@@ -2,23 +2,10 @@ angular.module("cines")
 
 .controller("filmesCtrl", ['$scope','$rootScope', 'RestService', function($scope, $rootScope,RestService) {
 
-	var filme = {
-		nome: "A volta dos que não foram (DUBLADO)",
-		imagem: "../../images/3.jpg.jpg",
-		genero: "Ação, Comédia e Aventura",
-		classificacao: "14 anos",
-		tempoDeDuracao: "2 horas e 14 minutos",
-		sinopse: "Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI.",
-		sessoes: [{
-						sala: 4,
-						horario: "14:30"
-					},
-					{
-						sala: 4,
-						horario: "19:00"
-					}
-		]
-	};
+	$scope.tiposDeFiltragem = {genero:"Genero",
+							classificacao:"Classificacao"};
+
+	$scope.tipoDeFiltragem = $scope.tiposDeFiltragem.genero;
 
 	$scope.selecionarFilme = function(filme){
 		$rootScope.filmeSelecionado = filme;
